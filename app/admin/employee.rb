db11beaaca5533c3fa21dcd09e98d05c724b1aa7
Kristin Beltz <beltz08@me.com>
2016-02-03 19:41:58 -0600

    Edit employee form to add text box for description

--- app/admin/employee.rb
+++ app/admin/employee.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Employee do
   permit_params :name, :description, :image, :image_cache
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :name
+      f.input :description, as: :text
+      f.input :image
+    end
+    f.actions
+  end
 end
