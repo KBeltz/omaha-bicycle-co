3b1a3ae7fb3b7a106a54ebd8c597c802b9f1b7a3
Kristin Beltz <beltz08@me.com>
2016-02-03 19:39:28 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Employee.create!(name: 'Sarah J.', description: 'I like bikes and think everybody should have one. Advocacy! #pedalhappy #shiftomaha #bikegang')
+Employee.create!(name: 'Jim P.', description: 'DH racer in a previous life, now exploring what Omaha has to offer after relocating here from New Jersey! You’ll see him atop the saddle of his Lynskey Sportive Disc or Surly Straggler, depending on the day. He’s been in the industry turning wrenches for over 20 years and has a real passion for solving complicated bike problems. Try him, he’ll likely find the solution to your weird bike problems…and maybe fabricate a part to aide in the process. Wizard. Fo real.')
+Employee.create!(name: 'Kelsey', description: 'Barista extraordinaire! She rides a sweet minty Straggler and will beat you up the hill. From slinging coffee behind the counter, to helping make #coffeeoutside on our Saturday morning rides, she’s a bike riding, coffee makin’ babe and we love her a lot.')
+Employee.create!(name: 'Bethany', description: 'This sweet gal is a total coffee geek and we love her for it! If you’ve tried our house-made syrups and enjoyed them, give her a high five for creating these dreamy concoctions. She’s an almost daily bike commuter and actress, to boot!')
+Employee.create!(name: 'Copper', description: 'Loves riding in her Burley Tail Wagon and chasing sticks (and food).')
+Employee.create!(name: 'Squirrel', description: 'Really into nuts.')
