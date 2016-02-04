416b9111d212e703995375d796289a3271a452f8
Kristin Beltz <beltz08@me.com>
2016-02-03 23:38:24 -0600

    Edit brands page

---
+++ app/assets/javascripts/brands.js
@@ -0,0 +1,12 @@
+// toggle button for bikes/parts
+$(function(){ $(document).foundation(); });
+
+function hideMe(id) {
+  var a = document.getElementById(id);
+  a.style.display = 'none';
+}
+
+function showMe(id) {
+  var b = document.getElementById(id);
+  b.style.display = 'block';
+}
