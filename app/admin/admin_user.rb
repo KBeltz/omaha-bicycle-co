1459b0eab4298597558198f827c598f4cd912472
Kristin Beltz <beltz08@me.com>
2016-02-03 19:21:49 -0600

    Controller override to keep user logged in when changing password

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,6 +1,28 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
+  controller do
+  def update
+    # get the currently logged in AdminUser's id
+    current_id = current_admin_user.id
+    # load the AdminUser being updated
+    @admin_user = AdminUser.find(params[:id])
+    # update the AdminUser with new attributes
+    # if successful, this will sign out the AdminUser being updated
+    if @admin_user.update_attributes(permitted_params[:admin_user])
+      # if the updated AdminUser was the currently logged in AdminUser, sign them back in
+      if @admin_user.id == current_id
+        sign_in(AdminUser.find(current_id), :bypass => true)
+      end
+      flash[:notice] = I18n.t('devise.passwords.updated_not_active')
+      redirect_to '/admin/admin_users'
+    # display errors
+    else
+      render :action => :edit
+    end
+  end
+  end
+
   index do
     selectable_column
     id_column
